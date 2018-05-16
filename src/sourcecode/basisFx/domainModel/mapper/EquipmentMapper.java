628f91006b2a42f5e150031bab8c8c9e2d9f84ea
AlexanderGulevich <iscander_02@mail.ru>
2018-05-16 21:56:48 +0200

    nnnn

---
+++ src/sourcecode/basisFx/domainModel/mapper/EquipmentMapper.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basisFx.domainModel.mapper;
+
+import basisFx.appCore.domainScetch.DomainObject;
+import basisFx.appCore.dataSource.DataMapper;
+import basisFx.appCore.dataSource.Db;
+import basisFx.domainModel.domaine.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author Alek
+ */
+public class EquipmentMapper extends DataMapper {
+    
+    private Equipment domainObject;
+    
+    private static EquipmentMapper instance;
+   
+    private EquipmentMapper() {}
+    
+    public static EquipmentMapper getInstance(){
+     
+        if(EquipmentMapper.instance!=null){
+            return EquipmentMapper.instance;
+        }else{
+            EquipmentMapper.instance=new EquipmentMapper();
+            return  EquipmentMapper.instance;
+        }
+        
+    }
+
+
+    @Override
+    public void insertDomainObject(DomainObject d) throws SQLException {
+        
+        domainObject=(Equipment) d;
+
+
+        if (isReadyToTransaction(d)) {
+
+                String expression = "INSERT INTO " + "Equipment"
+                        + "("
+                        + "name"
+                        + ") VALUES(?)";
+
+                PreparedStatement pstmt = Db.getConnection().prepareStatement(expression);
+                pstmt.setString(1, domainObject.getName());
+
+                pstmt.executeUpdate();
+
+        }
+
+    }
+
+    @Override
+    public void updateDomainObject(DomainObject d) throws SQLException {
+        if (isReadyToTransaction(d)) {
+
+                domainObject = (Equipment) d;
+
+                String expression = "UPDATE " + "Equipment"+
+                        " SET  name = ? "
+                        + "WHERE id= ?";
+
+
+                PreparedStatement pstmt = Db.getConnection().prepareStatement(expression);
+
+                pstmt.setString(1, domainObject.getName());
+                pstmt.setInt(2, domainObject.getId());
+
+
+                pstmt.executeUpdate();
+
+        }
+    }
+
+
+
+    @Override
+    public boolean isReadyToTransaction(DomainObject d) {
+        Equipment equipment= (Equipment) d;
+        if (equipment.getName()!=null
+                ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void getDomainList(ObservableList list)   {
+
+        String expression="SELECT * FROM " +"Equipment"+" ORDER BY ID";
+        
+        Statement stmt  = Db.getConnection().createStatement();
+        
+        ResultSet rs    = stmt.executeQuery(expression);
+
+            
+            while (rs.next()) { 
+                
+                Equipment pojo=new Equipment();
+                pojo.setId(rs.getInt("id"));
+                pojo.setName(rs.getString("name"));
+
+                
+                //вставляю id в список хранимых в бд
+                this.unitOfWork.getStoredPojoesId().add(rs.getInt("id"));
+                
+                list.add(pojo);
+
+            }
+    }
+
+    @Override
+    public void getDomainListForObserverTables(ObservableList list, DomainObject selectedDomainObject) {
+
+    }
+
+
+}
