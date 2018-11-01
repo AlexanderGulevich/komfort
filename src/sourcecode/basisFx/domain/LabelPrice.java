b75087b9b2efe38ecc1d74fbcb5b868822aaeb80
Alexander <iscander_02@mail.ru>
2018-11-01 15:38:21 +0300

    nn

--- src/sourcecode/basisFx/domain/LabelPrice.java
+++ src/sourcecode/basisFx/domain/LabelPrice.java
@@ -1,9 +1,47 @@
 package basisFx.domain;
 
+import basisFx.dataSource.Db;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.ObservableList;
 
+import java.sql.*;
+import java.time.LocalDate;
+
 public class LabelPrice extends ActiveRecord {
-    public LabelPrice(String entityName) {
+
+    private static LabelPrice INSTANCE = new LabelPrice();
+    private SimpleObjectProperty<String> price =new SimpleObjectProperty<>(this, "price", null);
+    private SimpleObjectProperty<LocalDate> startingDate =new SimpleObjectProperty<>(this, "startingDate", null);
+
+    public static LabelPrice getINSTANCE() {
+        return INSTANCE;
+    }
+
+    public String getPrice() {
+        return price.get();
+    }
+
+    public SimpleObjectProperty<String> priceProperty() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price.set(price);
+    }
+
+    public LocalDate getStartingDate() {
+        return startingDate.get();
+    }
+
+    public SimpleObjectProperty<LocalDate> startingDateProperty() {
+        return startingDate;
+    }
+
+    public void setStartingDate(LocalDate startingDate) {
+        this.startingDate.set(startingDate);
+    }
+
+    public LabelPrice() {
         super("LabelPrice");
     }
 
@@ -19,7 +57,30 @@ public class LabelPrice extends ActiveRecord {
 
     @Override
     public void update() {
+        String expression = "UPDATE "+    " LabelPrice"+ " SET  " +
+                " price = ?," +
+                " startDate = ?," +
+                " productId = ? " +
+                " where id =?";
 
+        boolean check = isUniquenessStartingDate(
+                findAllByOuterId(outerId),
+                activeRecord -> ((ProductPrice) activeRecord).getStartingDate(),
+                getStartingDate());
+
+        try {
+            if (!check) {
+                PreparedStatement pstmt = null;
+                pstmt = Db.connection.prepareStatement(expression);
+                pstmt.setDouble(1, Double.valueOf(price.get()));
+                pstmt.setDate(2, Date.valueOf(startingDate.get()));
+                pstmt.setInt(3, outerId);
+                pstmt.setInt(4, id.get());
+                pstmt.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -29,11 +90,51 @@ public class LabelPrice extends ActiveRecord {
 
     @Override
     public void insert() {
+        String expression = "INSERT INTO " + " LabelPrice "
+                + "("
+                + " price ,  "
+                + " startDate,  "
+                + " productId        "
+                + ") VALUES(?,?,?)";
+        boolean check = isUniquenessStartingDate(
+                findAllByOuterId(outerId),
+                activeRecord -> ((ProductPrice) activeRecord).getStartingDate(),
+                getStartingDate());
 
+        PreparedStatement pstmt;
+        if (check) {
+            try {
+                pstmt = Db.connection.prepareStatement(expression);
+                pstmt.setDouble(1, Double.valueOf(price.get()));
+                pstmt.setDate(2, Date.valueOf(startingDate.get()));
+                pstmt.setInt(3, outerId);
+
+                pstmt.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public ObservableList<ActiveRecord> findAllByOuterId(int id) {
-        return null;
+        ObservableList <ActiveRecord> list=createNewActiveRecordList();
+        String expression="SELECT * FROM " +" LabelPrice "+" where productId= " +id+" ORDER BY startDate desc";
+        try{
+            Statement stmt  = Db.connection.createStatement();
+            ResultSet rs    = stmt.executeQuery(expression);
+            while (rs.next()) {
+                ProductPrice pojo=new ProductPrice();
+                pojo.setId(rs.getInt("id"));
+                pojo.setPrice( Double.toString(rs.getDouble("price")));
+                pojo.setStartingDate(rs.getDate("startDate").toLocalDate());
+                list.add(pojo);
+            }
+        } catch (
+                SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+
     }
 }
