8ab79e4183951d68180568c99b02b141441e1d6d
Alexander <iscander_02@mail.ru>
2018-11-15 12:30:48 +0300

    nn

--- src/sourcecode/basisFx/appCore/mediators/MediatorTableAndCommonDate.java
+++ src/sourcecode/basisFx/appCore/mediators/MediatorTableAndCommonDate.java
@@ -1,11 +1,31 @@
 package basisFx.appCore.mediators;
 
 import basisFx.appCore.elements.AppNode;
+import basisFx.appCore.elements.ButtonWrapper;
+import basisFx.appCore.elements.DatePickerWrapper;
+import basisFx.appCore.elements.TableWrapper;
 import basisFx.domain.ActiveRecord;
 
+import java.time.LocalDate;
+
 public class MediatorTableAndCommonDate  extends Mediator{
+    private TableWrapper tableWrapper;
+    private DatePickerWrapper datePickerWrapper;
+    private ButtonWrapper buttonWrapper;
+
+    public static void setDate(LocalDate date) {
+        MediatorTableAndCommonDate.date = date;
+    }
+
     @Override
     public void inform(AppNode node) {
+        if (node==datePickerWrapper){
+
+        }
+        if (node==buttonWrapper){
+
+        }
+
 
     }
 
@@ -26,6 +46,18 @@ public class MediatorTableAndCommonDate  extends Mediator{
 
     @Override
     public void initElements() {
+        date=null;
+    }
 
+    public void setTableWrapper(TableWrapper tableWrapper) {
+        this.tableWrapper = tableWrapper;
+    }
+
+    public void setDatePickerWrapper(DatePickerWrapper datePickerWrapper) {
+        this.datePickerWrapper = datePickerWrapper;
+    }
+
+    public void setButtonWrapper(ButtonWrapper buttonWrapper) {
+        this.buttonWrapper = buttonWrapper;
     }
 }
