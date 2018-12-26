14552cdaf22d957fcee9f51fd25def2c49e0230c
AlexanderGulevich <iscander_02@mail.ru>
2018-12-26 06:21:47 +0300

    nn

---
+++ src/sourcecode/basisFx/appCore/windows/WindowImplInputForm.java
@@ -0,0 +1,24 @@
+package basisFx.appCore.windows;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class WindowImplInfo extends WindowImpl{
+
+    private  String messagge;
+
+    public WindowImplInfo(String messagge) {
+        super(530d, 300d, "Внимание!");
+        this.messagge=messagge;
+
+    }
+
+    @Override
+    public void init() {
+        Label label = (Label) windowAbstraction.getNode("label").getElement();
+        label.setText(titleName);
+        TextArea textArea = (TextArea) windowAbstraction.getNode("textArea").getElement();
+        textArea.setText(messagge);
+
+    }
+}
---
+++ src/sourcecode/basisFx/service/Service.java
@@ -0,0 +1,63 @@
+package basisFx.service;
+
+import basisFx.appCore.elements.AppNode;
+import basisFx.appCore.elements.TableWrapper;
+import basisFx.dataSource.UnitOfWork;
+import basisFx.domain.ActiveRecord;
+
+public class Servictor extends ServiceMediator {
+
+    private TableWrapper tableWrapper;
+
+    public void setTableWrapper(TableWrapper tableWrapper) {
+        this.tableWrapper = tableWrapper;
+    }
+
+    @Override
+    public void inform(AppNode node) {
+
+    }
+
+    @Override
+    public void wasRemoved(AppNode node, ActiveRecord record) {
+        UnitOfWork unitOfWork = ((TableWrapper) node).unitOfWork;
+        boolean readyToTransaction = record.isReadyToTransaction();
+        if (readyToTransaction) {
+            Boolean isNewDomane = ActiveRecord.isNewDomane(record);
+            if (!isNewDomane){
+                unitOfWork.registercDeleted(record.entityName,record);
+                unitOfWork.commit();
+            }
+        }
+    }
+
+    @Override
+    public void wasChanged(AppNode node, ActiveRecord record) {
+        UnitOfWork unitOfWork = ((TableWrapper) node).unitOfWork;
+        boolean readyToTransaction = record.isReadyToTransaction();
+        if (readyToTransaction) {
+            Boolean isNewDomane = ActiveRecord.isNewDomane(record);
+
+            if (!isNewDomane){
+                unitOfWork.registercDirty(record.entityName,record);
+            }else {
+                unitOfWork.registerNew(record.entityName,record);
+            }
+            unitOfWork.commit();
+            ((TableWrapper) node).getServiceMediator().refresh(node);
+
+        }
+    }
+
+    @Override
+    public void refresh(AppNode node) {
+        TableWrapper tableWrapper = (TableWrapper) node;
+        refreshTable(tableWrapper,tableWrapper.activeRecord.getAll());
+    }
+
+    @Override
+    public void initElements() {
+        tableWrapper.setItems(tableWrapper.activeRecord.getAll());
+    }
+
+}
